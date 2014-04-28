0044168aa15e282ad89f94795aecbb9dcaff858c
Luis Gil <lamaslg@gmail.com>
2014-04-28 19:19:59 +0200

    Primera subida

--- src/java/com/luis/servicios/ManagerEmpleados.java
+++ src/java/com/luis/servicios/ManagerEmpleados.java
@@ -20,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author luis
  */
-
+@Transactional
 public class ManagerEmpleados 
             extends HibernateDaoSupport{
     
